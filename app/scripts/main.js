f7f04d887bb61ca1dbff21eb39f69a0fe04c570b
Wendy Nichols <wendlelyn@gmail.com>
2014-10-09 21:15:02 -0400

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,8 @@
 
 
-var user = 'wendynichols'
-var url = 'https://api.github.com/users/'+ user +'?client_id=8e0c016d9fc699395719&client_secret=d1c7213dbdc2d4e37b0a3e819c9ab7f551ee4d5b';
+var url = 'https://api.github.com/users/wendynichols';
 
-var template = $('#user_stuff').html();
+var template = $('#user_stuff').html(),
     rendered = _.template(template);
 
 
@@ -12,7 +11,7 @@ $.getJSON(url).done(function (user_data) {
   //console.log(user_data.name);
   //as many statements that pertain to this 'user_data' as possible
 
-  $('.hero-unit').append(rendered(user_data));
+  $('.leftside').append(rendered(user_data));
 
 
 });
