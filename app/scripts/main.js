4e5b209df4eb66d32888ce09a073d2603b830dd9
Wendy Nichols <wendlelyn@gmail.com>
2014-10-12 12:52:28 -0400

    updates7

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,3 +22,26 @@ var temp = $('#heading').html(),
 $.getJSON(url).done(function (use) {
   $('.header').append(renderhead(use));
 });
+
+
+    //RIGHT SIDE TABS
+
+$(document).ready(function () {
+  $('.accordion-tabs-minimal').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+
+  $('.accordion-tabs-minimal').on('click', 'li > a', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs-minimal')
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+});
