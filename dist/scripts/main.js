961f983f49b982a9619db1b467af3923d9fb01e3
Wendy Nichols <wendlelyn@gmail.com>
2014-12-11 21:50:04 -0500

    fixed script tag

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -464,3 +464,53 @@ date : function(value, format) {
                                             (function($) {
                                               $.format = DateFormat.format;
                                             }(jQuery));
+
+var users = 'https://api.github.com/users/wendynichols';
+var repo ='https://api.github.com/users/wendynichols/repos';
+var org = 'https://api.github.com/users/wendynichols/orgs';
+var starred = 'https://api.github.com/users/wendynichols/starred';
+
+
+var template= $('#header').html();
+var rendered = _.template(template);
+$.getJSON(users).done( function(i){
+  $('.head_right').prepend(rendered(i));
+});
+
+var template2= $('#side_box').html();
+var rendered2 = _.template(template2);
+
+$.getJSON(users).done( function(i){
+  $('.sidebar').prepend(rendered2(i));
+});
+
+var template5= $('#star').html();
+var rendered5= _.template(template5);
+var star_count;
+
+$.getJSON(starred).done(function(starred_data){
+  star_count= starred_data.length;
+  var object ={
+    stargazers_count:star_count
+  };
+  $('.b').append(rendered5(object));
+});
+
+var template3= $('#org_img').html();
+var rendered3 = _.template(template3);
+
+$.getJSON(org).done( function(i){
+  i.forEach(function(x){
+    $('.org').append(rendered3(x));
+  });
+});
+
+var template4= $('#repo').html();
+var rendered4 = _.template(template4);
+
+$.getJSON(repo).done( function(i){
+  i.updated_at = moment(i.updated_at).fromNow();
+  i.forEach(function(x){
+    $('.bars2').append(rendered4(x));
+  });
+});
